ba5948f171defb656e8576fbb31345631bfe66d7
Agnieszka Florkowska <aga.florkowska99@gmail.com>
2023-01-01 22:54:59 +0100

    Added loading images from directory method

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
+import os
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 from PyQt5 import uic , QtGui
 
 
@@ -17,29 +18,31 @@ class MyGUI(QMainWindow):
         #Loading UI
         uic.loadUi("imageviewer.ui", self)
         
+        #Default settings
+        ### Default Windows
+        self.setWindowTitle("Medical Images Analyzer")
+        #self.setMinimumSize(QSize(300, 300))
+        
         #Defining widgets
         self.label = self.findChild(QLabel, "label")
         
         #Defining actions
         self.actionLoad_images.triggered.connect(self.load_image)
+        self.actionChoose_directory.triggered.connect(self.open_directory)
         
         
         #Defining variables
-        self.current_file = "healthy1.jpg"
+        self.current_file = "default.jpg"
         self.file_list = None
         self.file_counter = None
         
         
-        
-        #Default settings
-        ### Default Windows
-        self.setWindowTitle("Medical Images Analyzer")
-        
+    
         ### Default PixelLabel
         pixmap = QtGui.QPixmap(self.current_file)
         pixmap = pixmap.scaled(self.width(), self.height())
         self.label.setPixmap(pixmap)
-        self.label.setMinimumSize(1,1)
+        self.label.setMinimumSize(300,300)
         
         #Show the app
         self.show()
@@ -48,21 +51,40 @@ class MyGUI(QMainWindow):
         try:
             pixmap = QtGui.QPixmap(self.current_file)
         except:
-            pixmap = QtGui.QPixmap("healthy1.jpg") #ustawic jaks defaultowy 
+            pixmap = QtGui.QPixmap("default.jpg") 
+            
         pixmap = pixmap = pixmap.scaled(self.width(), self.height())
         self.label.setPixmap(pixmap)
         self.label.resize(self.width(), self.height())
         
     def load_image(self):
         options = QFileDialog().Options()
-        filename , _ = QFileDialog.getOpenFileName(self, "Open File", "", "Image Files (*.png, *.jpg)", options=options)
+        filename , _ = QFileDialog.getOpenFileName(self, "Open File", "", "Image Files (*.png)", options=options) 
         
-        if filename!= "":
+        if filename != "":
             self.current_file = filename
-            pixmap = QtGui.QPixmap(self.current_file)
-            pixmap = pixmap.scaled(self.width(), self.height())
-            self.label.setPixmap(pixmap)
-        
+        else:
+            self.current_file = "error_image.jpg" #ERROR 
+            
+        pixmap = QtGui.QPixmap(self.current_file)
+        pixmap = pixmap.scaled(self.width(), self.height())
+        self.label.setPixmap(pixmap)
+            
+    def open_directory(self):
+        directory = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        self.file_list = [directory + "/" + f for f in os.listdir(directory) if f.endswith(".jpg") or f.endswith(".png")]
+       
+        if len(self.file_list) > 0:
+            self.file_counter = 0
+            self.current_file = self.file_list[self.file_counter]
+        else:
+            self.current_file = "error_directory.jpg"
+            
+        pixmap = QtGui.QPixmap(self.current_file)
+        pixmap = pixmap.scaled(self.width(), self.height())
+        self.label.setPixmap(pixmap)         
+            
+             
 def main():
     app = QApplication([])
     win = MyGUI()
